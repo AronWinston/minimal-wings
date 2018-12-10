db7d5f5816b08c48dcf393ed874b08fb1d11a25c
Aron Winston <aronwinston2792@gmail.com>
2018-12-10 17:41:11 -0500

    final draft

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-// let items = document.createElement("img");
-// let info = document.createElement("p");
+
+let header = document.createElement("header");
 
 
 let head=document.createElement("img");
 
 head.style.display="flex";
-head.style.justifyContent="right";
+// head.style.justifyContent="space-around";
 head.src="http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c1e8.png";
-head.style.width="fit-content";
+head.style.width="600px";
 head.style.height="250px";
 head.style.fontSize="30px";
 head.style.border="4px solid yellow";
 head.style.backgroundColor="blue";
 head.style.margin="10px";
 head.style.padding="10px";
+
+
 
 
 
@@ -41,7 +43,7 @@ axios.get("https://www.redbullshopus.com/products.json")
         let items = document.createElement("img");
         let info = document.createElement("h1");
         let price = document.createElement("p");
-        let div = document.getElementsByClassName("container1")
+        
   
     
     info.innerHTML += data.products[i].title;
@@ -50,16 +52,14 @@ axios.get("https://www.redbullshopus.com/products.json")
 
     
     items.style.display="flex";
-    // items.style.justifyContent="center"
-    items.style.width="150px";
+    items.style.width="180px";
     items.style.border="1px solid red";
     info.style.fontSize="20px";
-    // info.style.border="2px dotted yellow";
-    // info.style.backgroundColor="grey";
     info.style.display="flex";
-    // info.style.justifyContent="center"
     price.style.display="flex";
+    price.style.padding="10px";
     price.style.justifyContent="left";
+    price.style.backgroundColor="yellow";
     
   
 
@@ -79,3 +79,9 @@ axios.get("https://www.redbullshopus.com/products.json")
 let body = document.getElementsByTagName('body')[0];
 body.style.backgroundColor="grey";
 body.appendChild(head);
+
+body.appendChild(header);
+header.appendChild(head);
+
+header.style.display ="flex";
+header.style.justifyContent="center";
